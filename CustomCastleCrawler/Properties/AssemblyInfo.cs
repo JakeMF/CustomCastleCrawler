a7e8f45118942d1809056116ab2eff17258e4e3d
JakeMF <39229964+JakeMF@users.noreply.github.com>
2019-03-04 11:54:36 -0500

    Updating Version Number (0.3.0.0)
    I forgot to increment the version in the previous build.

--- CustomCastleCrawler/Properties/AssemblyInfo.cs
+++ CustomCastleCrawler/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CustomCastleCrawler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Customizable Dungeon Crawler created using Windows Forms.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jacob Farley")]
 [assembly: AssemblyProduct("CustomCastleCrawler")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
